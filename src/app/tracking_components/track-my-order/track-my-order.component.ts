32ed1c67b1e80229fba32ad8773de5bf46b0e691
shalithadeshan <shalithad8520jayasekara@gmail.com>
2020-10-06 15:42:21 +0530

    Create cart, payment, track-my-order components

---
+++ src/app/tracking_components/track-my-order/track-my-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-track-my-order',
+  templateUrl: './track-my-order.component.html',
+  styleUrls: ['./track-my-order.component.css']
+})
+export class TrackMyOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
