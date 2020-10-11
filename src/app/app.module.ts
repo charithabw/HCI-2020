1c69aed751e7e6f8cf2358b31e42e88f10f87789
dilkithishaka <dilkithishaka1997@gmail.com>
2020-10-11 12:46:25 +0530

    Login page has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,18 @@ import { SellarUpdateComponent } from './Sellar_components/sellar-update/sellar-
 
 
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 import { AddBookComponent } from './Sellar_components/add-book/add-book.component';
+import { FormsModule } from '@angular/forms';
+//import { SigninComponentComponent } from './signin-component/signin-component.component';
+import { LoginUserComponent } from './Signin_component/login-user/login-user.component';
 
 
 @NgModule({
@@ -31,16 +42,28 @@ import { AddBookComponent } from './Sellar_components/add-book/add-book.componen
     SellarOrdersComponent,
     SellarUpdateComponent,
     AddBookComponent,
-    
+    //SigninComponentComponent,
+    LoginUserComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-   
+
     MatDialogModule,
     MatFormFieldModule,
-    
+
+    MatTableModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
