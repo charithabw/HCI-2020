bd57e36c2a4fc16d0be37fa5274d120c988253a8
charithabw <52483097+charithabw@users.noreply.github.com>
2020-10-16 20:21:15 +0530

    fixed changes

--- src/app/Buyer_component/personal-setting/personal-setting.component.ts
+++ src/app/Buyer_component/personal-setting/personal-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal-setting',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalSettingComponent implements OnInit {
 
+  updateForm = new FormGroup({
+    fname: new FormControl('', Validators.required),
+    lname: new FormControl('', Validators.required),
+    dname: new FormControl('', Validators.required),
+    adders: new FormControl('', Validators.required),
+  })
+    get fname(){return this.updateForm.get('fname')}
+    get lname(){return this.updateForm.get('lname')}
+    get dname(){return this.updateForm.get('dname')}
+    get addres(){return this.updateForm.get('adders')}
   constructor() { }
 
   ngOnInit(): void {
